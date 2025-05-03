8625b6e8bfcfb4ea192a11591d21549c1f90dcb9
Luomy EQua <equality560@gmail.com>
2025-05-03 15:47:54 +0100

    Initial Commit

---
+++ AuthSDKTests/AuthSDKTests.swift
@@ -0,0 +1,17 @@
+//
+//  AuthSDKTests.swift
+//  AuthSDKTests
+//
+//  Created by Richard Uzor on 03/05/2025.
+//
+
+import Testing
+@testable import AuthSDK
+
+struct AuthSDKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
