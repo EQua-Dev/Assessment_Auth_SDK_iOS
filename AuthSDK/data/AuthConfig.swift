a9a03c34fec10175616405fd01c7abe4eaf1e6a3
Luomy EQua <equality560@gmail.com>
2025-05-03 19:13:29 +0100

    adjusted color parameters

--- AuthSDK/data/AuthConfig.swift
+++ AuthSDK/data/AuthConfig.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 public struct AuthConfig {
-    public var primaryColor: Color = .blue
-    public var backgroundColor: Color = .white
-    public var textColor: Color = .black
-    public var font: Font = .system(size: 16)
-    public var submitButtonText: String = "Submit"
-    public var showUsername: Bool = true
-    
-    public init() {}
-}
+    public var primaryColor: Color
+    public var backgroundColor: Color
+    public var textColor: Color
+    public var font: Font
+    public var submitButtonText: String
+    public var showUsername: Bool
 
+    // Default init
+    public init() {
+        self.primaryColor = .blue
+        self.backgroundColor = .white
+        self.textColor = .black
+        self.font = .system(size: 16)
+        self.submitButtonText = "Submit"
+        self.showUsername = true
+    }
+
+    // New init using hex strings
+    public init(
+        primaryColorHex: String,
+        backgroundColorHex: String,
+        textColorHex: String,
+        submitButtonText: String,
+        showUsername: Bool
+    ) {
+        self.primaryColor = Color(hex: primaryColorHex) ?? .blue
+        self.backgroundColor = Color(hex: backgroundColorHex) ?? .white
+        self.textColor = Color(hex: textColorHex) ?? .black
+        self.font = .system(size: 16)
+        self.submitButtonText = submitButtonText
+        self.showUsername = showUsername
+    }
+}
